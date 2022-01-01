0e35731bd806f19148d583cbebdb2bce2531d52e
Faraz Abidi <faraz.abidi@gmail.com>
2021-12-31 19:37:47 -0800

    Implemented crown scrolling.

--- Extrasensory WatchKit Extension/UrgeView.swift
+++ Extrasensory WatchKit Extension/UrgeView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct UrgeView: View {
+    @State var goalIndex: Double = 0
+
     var body: some View {
         VStack{
-            Text("Nail Biting")
+            Text("Scroll: \(goalIndex)")
+                .focusable(true)
+                .digitalCrownRotation($goalIndex, from: 0, through: 5, by: 1, sensitivity: .low, isContinuous: true) //TODO: Use WKInterfacePicker later
+            
             Button(action: {}){
                 Text("Urge")
                     .font(.largeTitle)
                     .fontWeight(.thin)
                     .padding(.vertical, 30) //TODO: Don't hardcode the number
             }
-            
-            
+   
         }
     }
 }
