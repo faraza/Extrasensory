c90e2e1cb787fd2181fbfe84621a9ee2a0549dba
Faraz Abidi <faraz.abidi@gmail.com>
2022-01-06 16:19:11 -0800

    Looks like we've solved the data loss problem. It's not a great solution - ideal would be hanging onto the data while loading is in progress - but it's a start. At least we won't be losing data.

--- Shared/Models/XSEventsStore.swift
+++ Shared/Models/XSEventsStore.swift
@@ -10,7 +10,7 @@ import os
 
 class XSEventsStore: ObservableObject{
     @Published var events: [XSEvent] = []
-    private static var lastSavedEventsCount: Int = 0
+    private static var loadComplete = false
     
     init(){
         let nc = NotificationCenter.default
@@ -56,7 +56,7 @@ class XSEventsStore: ObservableObject{
                     completion(.success(savedEvents))
 //                    completion(.success(XSEvent.sampleData))
 //                    completion(.success([]))
-                    lastSavedEventsCount = savedEvents.count
+                    loadComplete = true
                     os_log("%@", type: .info, "Loaded \(fileURL). Length: \(savedEvents.count)")
                 }
             } catch {
@@ -72,8 +72,8 @@ class XSEventsStore: ObservableObject{
     }
     
     static func save(events: [XSEvent], completion: @escaping (Result<Int, Error>)->Void) {
-        if(events.count < lastSavedEventsCount - 10){
-            os_log("%@", type: .error, "Current event count is much lower than saved. To safeguard against data loss, preventing save action. Saved count: \(lastSavedEventsCount) New count: \(events.count)")
+        if(!loadComplete){
+            os_log("ERROR - Loading note complete yet. Preventing save.")
             completion(.failure(FileSaveError.dataLossSafeguard))
         }
         DispatchQueue.global(qos: .background).async {
@@ -84,7 +84,6 @@ class XSEventsStore: ObservableObject{
                 DispatchQueue.main.async {
                     completion(.success(events.count))
                     os_log("%@", type: .info, "Saved \(outfile). Length: \(events.count)")
-                    lastSavedEventsCount = events.count
                 }
             } catch {
                 DispatchQueue.main.async {
