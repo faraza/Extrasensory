869a8f7fca67ad3216f4577cba156ec2b0724af2
Faraz Abidi <faraz.abidi@gmail.com>
2022-01-09 14:39:28 -0800

    Created inactive list.

--- Extrasensory/Views/GoalsList.swift
+++ Extrasensory/Views/GoalsList.swift
@@ -8,19 +8,28 @@
 import SwiftUI
 
 struct GoalsList: View {
-    @State var goals: [String]
-    
-    func move(from source: IndexSet, to destination: Int) {
-        goals.move(fromOffsets: source, toOffset: destination)
-    }
+    @State var activeGoals: [String]
+    @State var inactiveGoals: [String]        
     
     var body: some View {
         NavigationView{
             List{
-                ForEach(goals, id: \.self){ goal in
-                    Text(goal)
+                Section(header: Text("Active Goals")){
+                    ForEach(activeGoals, id: \.self){ goal in
+                        Text(goal)
+                    }
+                    .onMove{source, destination in
+                        activeGoals.move(fromOffsets: source, toOffset: destination)
+                    }
                 }
-                .onMove(perform: move)
+                Section(header: Text("Inactive Goals")){
+                    ForEach(inactiveGoals, id: \.self){ goal in
+                        Text(goal)
+                    }
+                    .onMove{source, destination in
+                        inactiveGoals.move(fromOffsets: source, toOffset: destination)
+                    }
+                }
             }
             .toolbar{
                 EditButton()
@@ -30,8 +39,9 @@ struct GoalsList: View {
 }
 
 struct GoalsList_Previews: PreviewProvider {
-    static let goals = ["Biting Nails", "Browse", "Judgmental Thoughts"]
+    static let activeGoals = ["Biting Nails", "Browse", "Judgmental Thoughts"]
+    static let inactiveGoals = ["Video Games", "Karate"]
     static var previews: some View {
-        GoalsList(goals: goals)
+        GoalsList(activeGoals: activeGoals, inactiveGoals: inactiveGoals)
     }
 }
