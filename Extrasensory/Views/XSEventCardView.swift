6b67306ae33fc65cf8b33be8b0355875bb005862
Faraz Abidi <faraz.abidi@gmail.com>
2022-01-10 18:33:54 -0800

    Now showing goal name in list instead of id.

--- Extrasensory/Views/XSEventCardView.swift
+++ Extrasensory/Views/XSEventCardView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct XSEventCardView: View {
     let event: XSEvent
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(entity: Goal.entity(), sortDescriptors: [NSSortDescriptor(key: "activeListPosition", ascending: false)])
+    private var allGoals: FetchedResults<Goal>
+    
+    func getGoalNameFromKey(goalKey: String? = nil)->String{
+        if let unwrappedKey = goalKey{
+            return allGoals.first(where: {
+                $0.identifierKey == unwrappedKey
+            })?.shortName ?? "GOAL NAME NOT FOUND"
+        }
+        
+        return "GOAL NAME NOT FOUND"
+    }
+    
     var body: some View {
         HStack{
-            Text(event.goalKey!)
+            Text(getGoalNameFromKey(goalKey: event.goalKey))
                 .font(.headline)                
                 .accessibilityLabel("goal")
             Text(event.urgeFamilyType!)
